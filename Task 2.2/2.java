d6ac97465c82563976c34a36a9fc7dae63ebb538
Denis Kamyshov <denis_kamyshov@epam.com>
2019-06-25 16:55:59 +0300

    Update 2.java

--- Task 2.2/2.java
+++ Task 2.2/2.java
@@ -8,29 +8,56 @@ public static void main(String[] args) {
  
 	HashSet<Integer> hashSet = new HashSet<Integer>();
 	TreeSet<Integer> treeSet = new TreeSet<Integer>();
+	
+	// start time
+	long startTimeHSA = System.nanoTime();
+    	for (int i = 0; i < 1000; i++) {
+	    	int x = r.nextInt(1000 - 10) + 10;
+	    	hashSet.add(new Integer(x));
+	    }
+	// end time
+	long endTimeHSA = System.nanoTime();
+	long durationHSA = endTimeHSA - startTimeHSA;
+	
+	System.out.println("HashSet add: " + durationHSA);
+	
+		// start time
+	long startTimeHSR = System.nanoTime();
+ 	    for (int i = 0; i < 1000; i++) {
+		    int x = r.nextInt(1000 - 10) + 10;
+		    hashSet.remove(new Integer(x));
+	    }
+	// end time
+	long endTimeHSR = System.nanoTime();
+	long durationHSR = endTimeHSR - startTimeHSR;
+	
+	System.out.println("HashSet remove: " + durationHSR);
+	
+	
 
 	// start time
-	long startTime = System.nanoTime();
- 
-	for (int i = 0; i < 1000; i++) {
-		int x = r.nextInt(1000 - 10) + 10;
-		hashSet.add(new Integer(x));
-	}
+	long startTimeTSA = System.nanoTime();
+	    for (int i = 0; i < 1000; i++) {
+		    int x = r.nextInt(1000 - 10) + 10;
+		    treeSet.add(new Integer(x));
+	    }
 	// end time
-	long endTime = System.nanoTime();
-	long duration = endTime - startTime;
-	System.out.println("HashSet: " + duration);
- 
+	long endTimeTSA = System.nanoTime();
+	long durationTSA = endTimeTSA - startTimeTSA;
+	
+	System.out.println("TreeSet add: " + durationTSA);
+
 	// start time
-	startTime = System.nanoTime();
-	for (int i = 0; i < 1000; i++) {
-		int x = r.nextInt(1000 - 10) + 10;
-		treeSet.add(new Integer(x));
-	}
+    long startTimeTSR = System.nanoTime();
+	    for (int i = 0; i < 1000; i++) {
+		    int x = r.nextInt(1000 - 10) + 10;
+		    treeSet.add(new Integer(x));
+	    }
 	// end time
-	endTime = System.nanoTime();
-	duration = endTime - startTime;
-	System.out.println("TreeSet: " + duration);
+	long endTimeTSR = System.nanoTime();
+	long durationTSR = endTimeTSR - startTimeTSR;
+	
+	System.out.println("TreeSet remove: " + durationTSR);
  
 }
 }
